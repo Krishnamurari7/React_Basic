234862c84a1a6c1545bc23f4e5db80960f534e64
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-02-01 13:58:58 +0530

    app update

--- resturent/src/App.js
+++ resturent/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-// import Resturant from "./component/Basics/Resturent";
-import UseState from "./component/Hooks/useState";
-import UseEffect from "./component/Hooks/useEffect";
-import UseReducer from "./component/Hooks/useReducer";
+import Resturant from "./component/Basics/Resturent";
 
 const App = () => {
-  return <UseState />;
-  // return <Resturant />;
+  // return <UseState />;
+  return <Resturant />;
 };
 
 export default App;

effc1bbb30aa8e1198a19e9c2ecf70d7d54f66c8
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-02-27 11:40:10 +0530

    Update App.js

--- increament/src/App.js
+++ increament/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-   <div className='justify-center align-content-center'>
-   <div className="flex flex-col bg-slate-800 justify-center text-white w-[60vw] h-[30vh]">
+   <div className='justify-center align-content-center p-60'>
+   <div className="flex flex-col bg-slate-800 align-content-center  text-white w-[40vw] h-[40vh] border-rose-400 rounded-lg">
    <h1>Increament AND Decrement</h1>
    <div className="flex gap-5">
     <div className="dec">-</div>

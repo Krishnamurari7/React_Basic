cb0208df30c0d527a703a67162ec6864a7f28176
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-02-27 11:33:38 +0530

    new basic project start

---
+++ increament/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <div className='justify-center align-content-center'>
+   <div className="flex flex-col bg-slate-800 justify-center text-white w-[60vw] h-[30vh]">
+   <h1>Increament AND Decrement</h1>
+   <div className="flex gap-5">
+    <div className="dec">-</div>
+    <div className="val">Value</div>
+    <div className="inc">+</div>
+   </div>
+   </div>
+   </div>
+  );
+}
+
+export default App;

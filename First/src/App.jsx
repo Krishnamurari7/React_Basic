705568b2a01c754a99c6c285365d003ed1cc8530
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-06-17 07:33:53 +0530

    setup and start

--- First/src/App.jsx
+++ First/src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import Header from './Component/Header/Header'
+import Hero from './Component/Hero/Hero'
 
 function App() {
 
   return (
     <>
     <h1>Begginer to advance</h1>
+    <Header />
+    <Hero />
     </>
   )
 }

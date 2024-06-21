64278e80ab3b59aa335ca219d26d6136b0b7b643
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-06-21 06:41:36 +0530

    update

--- Second/src/Component/ItemData/ItemData.jsx
+++ Second/src/Component/ItemData/ItemData.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import './ItemData.css'
 
 const ItemData = (props) => {
+  const date = props.date;
+  const month = props.month;
+  const year = props.year;
   return (
     <div className='itemdata'>
-        <div>{props.date}</div>
-        <div>{props.month} </div>
-        <div>{props.year}</div>
+        <span>{date}</span>
+        <span>{month} </span>
+        <span>{year}</span>
       
     </div>
   )
